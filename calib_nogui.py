76ad7e7dbc6e9b3f733ab8adce6b22290170097c
JC-OP914 <JC-OP914@github.com>
2026-07-29 19:07:09 +0800

    换成320, 240版本

--- calib_nogui.py
+++ calib_nogui.py
@@ -10,13 +10,13 @@ import argparse
 
 # 参数配置
 INPUT_PATH = '/run/user/1000/gvfs/gphoto2:host=Kendryte_CanMV_001000000/store_ffff0002/Photos/'
-IMAGE_FILE = 'calib_1280x720'
+IMAGE_FILE = '.png'
 CAMERA_TYPE = 'normal'
-FRAME_WIDTH = 1280
-FRAME_HEIGHT = 720
+FRAME_WIDTH = 320
+FRAME_HEIGHT = 240
 BORAD_WIDTH = 9   # 内角点宽度
 BORAD_HEIGHT = 6  # 内角点高度
-SQUARE_SIZE = 11  # 方格边长(mm)
+SQUARE_SIZE = 10  # 方格边长(mm)
 CALIB_NUMBER = 30 # 最少需要的有效标定帧数
 SUBPIX_REGION = 5
 
